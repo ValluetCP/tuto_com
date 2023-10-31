52f447ca8fb7b44314a45addb8ca6b7e858f80ef
Cynthia <cynthia.petitot@gmail.com>
2023-10-31 14:15:40 +0100

    PHP -Les rois de France

---
+++ php/partie_2/index.php
@@ -0,0 +1,100 @@
+<!-- PARTIE 2 : LES ROIS DE FRANCE -->
+
+<!-- 1 page web = 4 pages web ! -->
+
+<?php
+$histoire = "<p>Accedebant enim eius asperitati, ubi inminuta vel laesa amplitudo imperii dicebatur, et iracundae suspicionum quantitati proximorum cruentae blanditiae exaggerantium incidentia et dolere inpendio simulantium, si principis periclitetur vita, a cuius salute velut filo pendere statum orbis terrarum fictis vocibus exclamabant.</p>";
+if(isset($_GET['rois'])){
+$rois = $_GET['rois'];
+
+
+switch ($rois) {
+    case 1:
+        $histoire = "<h1>François 1er</h1>
+        <img src=\"./img/francois-1.jpg\" class=\"imgRoi\">
+        <p>Quare talis improborum consensio non modo excusatione amicitiae tegenda non est sed potius supplicio omni vindicanda est, ut ne quis concessum putet amicum vel bellum patriae inferentem sequi; quod quidem, ut res ire coepit, haud scio an aliquando futurum sit. Mihi autem non minori curae est, qualis res publica post mortem meam futura, quam qualis hodie sit.
+
+        Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit.
+        </p>
+        <p>
+            Vita est illis semper in fuga uxoresque mercenariae conductae ad tempus ex pacto atque, ut sit species matrimonii, dotis nomine futura coniunx hastam et tabernaculum offert marito, post statum diem si id elegerit discessura, et incredibile est quo ardore apud eos in venerem uterque solvitur sexus.
+        </p>
+        <p>Quare talis improborum consensio non modo excusatione amicitiae tegenda non est sed potius supplicio omni vindicanda est, ut ne quis concessum putet amicum vel bellum patriae inferentem sequi; quod quidem, ut res ire coepit, haud scio an aliquando futurum sit. Mihi autem non minori curae est, qualis res publica post mortem meam futura, quam qualis hodie sit.
+
+        Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit.
+        </p>
+        <p>
+            Vita est illis semper in fuga uxoresque mercenariae conductae ad tempus ex pacto atque, ut sit species matrimonii, dotis nomine futura coniunx hastam et tabernaculum offert marito, post statum diem si id elegerit discessura, et incredibile est quo ardore apud eos in venerem uterque solvitur sexus.
+        </p>";
+        break;
+    case 2:
+        $histoire = "<h1>Charles IX</h1>
+        <img src=\"./img/charles-9.jpg\" class=\"imgRoi\">
+        <p>Quare talis improborum consensio non modo excusatione amicitiae tegenda non est sed potius supplicio omni vindicanda est, ut ne quis concessum putet amicum vel bellum patriae inferentem sequi; quod quidem, ut res ire coepit, haud scio an aliquando futurum sit. Mihi autem non minori curae est, qualis res publica post mortem meam futura, quam qualis hodie sit.
+
+        Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit.
+        </p>
+        <p>
+            Vita est illis semper in fuga uxoresque mercenariae conductae ad tempus ex pacto atque, ut sit species matrimonii, dotis nomine futura coniunx hastam et tabernaculum offert marito, post statum diem si id elegerit discessura, et incredibile est quo ardore apud eos in venerem uterque solvitur sexus.
+        </p>";
+        break;
+    case 3:
+        $histoire = "<h1>Louis XIV</h1>
+        <img src=\"./img/louis-14.jpg\" class=\"imgRoi\">
+        <p>Quare talis improborum consensio non modo excusatione amicitiae tegenda non est sed potius supplicio omni vindicanda est, ut ne quis concessum putet amicum vel bellum patriae inferentem sequi; quod quidem, ut res ire coepit, haud scio an aliquando futurum sit. Mihi autem non minori curae est, qualis res publica post mortem meam futura, quam qualis hodie sit.
+
+        Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit.
+        </p>
+        <p>
+            Vita est illis semper in fuga uxoresque mercenariae conductae ad tempus ex pacto atque, ut sit species matrimonii, dotis nomine futura coniunx hastam et tabernaculum offert marito, post statum diem si id elegerit discessura, et incredibile est quo ardore apud eos in venerem uterque solvitur sexus.
+        </p>";
+        break;
+    
+    default:
+        # code...
+        break;
+}
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Rois de France</title>
+</head>
+<body>
+    <div id="site">
+        <header>Les rois de France</header>
+        <nav>
+            <div class="navRoi">
+                <a href="?rois=1">
+                    <img src="./img/francois-1.jpg" alt="" class="avatar">
+                </a>
+                <h2>François 1er</h2>
+                1494 - 1547
+            </div>
+            <div class="navRoi">
+                <a href="?rois=2">
+                    <img src="./img/charles-9.jpg" alt="" class="avatar">
+                </a>
+                <h2>Charles IX</h2>
+                1550 - 1574
+            </div>
+            <div class="navRoi">
+                <a href="?rois=3">
+                    <img src="./img/louis-14.jpg" alt="" class="avatar">
+                </a>
+                <h2>Louis XIV</h2>
+                1638 - 1715
+            </div>
+        </nav>
+        <article>
+            <?= $histoire ?>
+        </article>
+        <footer>Histoire de France</footer>
+    </div>
+</body>
+</html>
