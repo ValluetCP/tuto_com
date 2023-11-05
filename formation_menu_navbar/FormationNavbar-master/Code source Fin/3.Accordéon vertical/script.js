4594f1555ad8c652f30c6b5410df73bf03cc54df
Cynthia <cynthia.petitot@gmail.com>
2023-11-05 11:34:58 +0100

    Formation menu navbar

--- formation_menu_navbar/FormationNavbar-master/Code source Fin/3.Accordéon vertical/script.js
+++ formation_menu_navbar/FormationNavbar-master/Code source Fin/3.Accordéon vertical/script.js
@@ -6,11 +6,13 @@ allItems.forEach(item => {
 
     item.addEventListener('click', () => {
 
+        // ajoute la class active
         item.classList.toggle('active')
 
 
         for(i = 0; i < chevron.length; i++) {
-
+            
+            // faire baisser le chevron du bon li
             if(chevron[i].getAttribute('data-anim') === item.getAttribute('data-anim')){
 
                 chevron[i].classList.toggle('icones-active');
@@ -23,6 +25,7 @@ allItems.forEach(item => {
 
 })
 
+// dès que l'on clique sur l'enfant, il s'arrête(ne lance pas l'évènement du parent) et peut mtn être sélectionné. On peut mtn appuyer sur le lien a sans que le menu ne se referme
 enfants.forEach(enfant => {
     enfant.addEventListener('click', (e) => {
         e.stopPropagation();
--- formation_menu_navbar/FormationNavbar-master/Code source Fin/5.MenuRond/script.js
+++ formation_menu_navbar/FormationNavbar-master/Code source Fin/5.MenuRond/script.js
@@ -10,10 +10,11 @@ btn.addEventListener('click', () => {
 
     allItems.forEach(item => {
 
+        // faire apparaître les icônes...
         item.classList.toggle('apparition')
-
+        // contains vérifie s'il a la class apparition
         if(item.classList.contains('apparition') === true){
-
+            // ...avec un temps d'attente
             setTimeout(() => {
                 item.classList.toggle('real');
             }, 200)
@@ -31,7 +32,7 @@ allItems.forEach(item => {
 
         btn.classList.toggle('active');
         ligne.classList.toggle('active-ligne');
-
+        // enlevé la class apparition aux icones
         allItems.forEach(item => {
             item.classList.toggle('real')
             item.classList.toggle('apparition')
