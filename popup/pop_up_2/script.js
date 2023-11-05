b4ff849ffad3da2bacca7a9c3ed0635f6df46135
Cynthia <cynthia.petitot@gmail.com>
2023-11-05 16:24:41 +0100

    projet : POPUP 2

---
+++ popup/pop_up_2/script.js
@@ -0,0 +1,14 @@
+
+var conteneur = document.getElementById('conteneur');
+
+function open_conteneur(){
+    conteneur.style.visibility = 'visible';
+}
+
+function close_conteneur(){
+    conteneur.style.visibility = 'hidden';
+}
+
+// function closeFenetre(){
+//     fenetre.style.top = '-600px'
+// }
