29f782e821e2f500f49dac038fe35181b173f457
Cynthia <cynthia.petitot@gmail.com>
2023-11-05 15:14:40 +0100

    projet :  HTACCESS et POPUP 1

---
+++ popup/pop_up_1/glisse_left/script.js
@@ -0,0 +1,19 @@
+
+var fenetre = document.getElementById('fenetre');
+function openFenetre(){
+    fenetre.style.left = '0px'
+}
+
+function closeFenetre(){
+    fenetre.style.left = '-80%'
+}
+
+
+// AUTRE SYNTAXE
+// function openFenetre(){
+//     document.getElementById('fenetre').style.top = '0px'
+// }
+
+// function closeFenetre(){
+//     document.getElementById('fenetre').style.top = '-600px'
+// }
---
+++ popup/pop_up_1/glisse_top/script.js
@@ -0,0 +1,19 @@
+
+var fenetre = document.getElementById('fenetre');
+function openFenetre(){
+    fenetre.style.top = '0px'
+}
+
+function closeFenetre(){
+    fenetre.style.top = '-600px'
+}
+
+
+// AUTRE SYNTAXE
+// function openFenetre(){
+//     document.getElementById('fenetre').style.top = '0px'
+// }
+
+// function closeFenetre(){
+//     document.getElementById('fenetre').style.top = '-600px'
+// }
